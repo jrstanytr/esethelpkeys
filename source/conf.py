a9f1d59ab4f3f8caf9bfa3147dc0f11acebbce77
jrstanytr <jrstanytr@gmail.com>
2026-01-09 21:29:50 +0530

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "esethelpkeys Documentation"
+copyright = '© 2026 esethelpkeys. All Rights Reserved.
+'
+author = 'jrstanytr'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+def setup(app):
+    app.add_css_file('theme_override.css')
